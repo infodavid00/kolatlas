71e03d585541b05032e07a1a7be91d66c1e4cb9a
infodavid00 <da40au40@gmail.com>
2024-09-23 19:01:58 +0100

    deployed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import db from "./src/etc/db.conf.js";
-import root from "./src/interface/root.js";
-import records from "./src/interface/records.js";
+import db from "./src/etc/db.conf.js"; 
+import root from "./src/interface/root.js"; 
+import records from "./src/interface/records.js"; 
+import { onRequest } from "firebase-functions/v2/https";
 
 const application = express();
 
@@ -16,4 +17,6 @@ application.use("/records", records);
 
 await db(process.env.DBKEY).connect();
 
-application.listen(8080, () => console.log("started on :8080"));
+export const api = onRequest((request, response) => {
+  application(request, response);
+});
